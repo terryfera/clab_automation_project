95b2a533151e8edbce8fdfc28e35dc89d21b74ef
christianperauer <christianperauer@yahoo.com>
2022-11-21 17:21:55 -0500

    Update clab_send_cmd_test.py
    Updated Testing Script for clab spinup test using python

--- clab_send_cmd_test.py
+++ clab_send_cmd_test.py
@@ -1,3 +1,23 @@
 import os
 import subprocess
 
+user_selection = input('Enter your Oprtion: ')
+
+def execute_clab_cmd(user_opt):
+    # CLAB run command
+    cmd_up = 'containerlab deploy -t arista.labtest.yml'
+    cmd_down = 'containerlab destroy -t arista.labtest.yml --graceful'
+    if user_opt == 1:
+        temp = subprocess.Popen([cmd_up], stdout= subprocess.PIPE)
+        output = str(temp.communicate())
+        return output
+    elif user_opt == 2:
+        temp = subprocess.Popen([cmd_down], stdout= subprocess.PIPE)
+        output = str(temp.communicate())
+        return output
+    else:
+        return 'Error or Invalid option'
+
+print(f'You Selected Option:  {user_selection}')
+
+execute_clab_cmd(user_selection)
