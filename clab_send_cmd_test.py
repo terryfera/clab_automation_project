fc3eb6f36ce0d83c00be6b044f5d8ff5f9ca2a3e
christianperauer <christianperauer@yahoo.com>
2022-11-22 11:58:00 -0500

    Update clab_send_cmd_test.py
    Applying testing learnings from conditional and trying on functions again

--- clab_send_cmd_test.py
+++ clab_send_cmd_test.py
@@ -7,12 +7,12 @@ def execute_clab_cmd(user_opt):
     # CLAB run command
     #cmd_up = 'containerlab deploy -t arista.labtest.yml'
     #cmd_down = 'containerlab destroy -t arista.labtest.yml --graceful'
-    if user_opt == 1:
-        temp = subprocess.run(['conteinerlab', 'deploy', '-t', 'arista.labtest.yml'], stdout= subprocess.PIPE, text=True)
+    if user_opt == '1':
+        temp = subprocess.run('containerlab deploy -t arista.labtest.yml', capture_output=True, text=True, shell=True)
         output = str(temp.communicate())
         return output
-    elif user_opt == 2:
-        temp = subprocess.run(['conteinerlab', 'destroy', '-t', 'arista.labtest.yml', '--graceful'], stdout= subprocess.PIPE, text=True)
+    elif user_opt == '2':
+        temp = subprocess.run('containerlab destroy -t arista.labtest.yml --graceful', capture_output=True, text=True, shell=True)
         output = str(temp.communicate())
         return output
     else:
