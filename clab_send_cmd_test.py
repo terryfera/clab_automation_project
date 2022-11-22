6c014284344c0cacfebe6cee51881512c8791161
christianperauer <christianperauer@yahoo.com>
2022-11-22 06:06:33 -0500

    Reformat test script
    Testing Subprocess, with more arguments

--- clab_send_cmd_test.py
+++ clab_send_cmd_test.py
@@ -5,14 +5,14 @@ user_selection = input('Enter your Oprtion: ')
 
 def execute_clab_cmd(user_opt):
     # CLAB run command
-    cmd_up = 'containerlab deploy -t arista.labtest.yml'
-    cmd_down = 'containerlab destroy -t arista.labtest.yml --graceful'
+    #cmd_up = 'containerlab deploy -t arista.labtest.yml'
+    #cmd_down = 'containerlab destroy -t arista.labtest.yml --graceful'
     if user_opt == 1:
-        temp = subprocess.Popen([cmd_up], stdout= subprocess.PIPE)
+        temp = subprocess.run(['conteinerlab', 'deploy', '-t', 'arista.labtest.yml'], stdout= subprocess.PIPE, text=True)
         output = str(temp.communicate())
         return output
     elif user_opt == 2:
-        temp = subprocess.Popen([cmd_down], stdout= subprocess.PIPE)
+        temp = subprocess.run(['conteinerlab', 'destroy', '-t', 'arista.labtest.yml', '--graceful'], stdout= subprocess.PIPE, text=True)
         output = str(temp.communicate())
         return output
     else:
